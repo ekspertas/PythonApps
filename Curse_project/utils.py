1b44458f7bcc9905435b864b24df29f440bff8d8
ekspertas <ekspertasd@gmail.com>
2022-01-06 15:54:21 +0200

    дополнение ДЗ к уроку 3

--- Curse_project/utils.py
+++ Curse_project/utils.py
@@ -1,11 +1,11 @@
+import json
 import os
 import sys
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 
-def load_config(is_server=True):
+def load_configs(is_server=True):
     config_keys = [
-        'DEFAULT_IP_ADDRESS',
         'DEFAULT_PORT',
         'MAX_CONNECTIONS',
         'MAX_PACKAGE_LENGTH',
@@ -20,19 +20,35 @@ def load_config(is_server=True):
     ]
     if not is_server:
         config_keys.append('DEFAULT_IP_ADDRESS')
-    if not os.path.exists('.env'):
-        print('Файл конфигурации не найден')
-        sys.exit(1)
 
     CONFIGS= {}
-    # load_dotenv(dotenv_path=os.path)
-    for key in config_keys:
-        CONFIGS[key] = os.getenv(key)
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+        for key in config_keys:
+            CONFIGS[key] = os.getenv(key)
+            if os.getenv(key) is None:
+                print(f'В файле конфигурации не хватает ключа: {key}')
+                sys.exit(1)
+    else:
+        print('Файл конфигурации не найден')
+        sys.exit(1)
 
     return CONFIGS
 
 
-a = load_config()
-print(a)
-b = os.getenv('ERROR')
-print(b)
+def send_message(opened_socket, message, CONFIGS):
+    json_message = json.dumps(message)
+    response = json_message.encode(CONFIGS.get('ENCODING'))
+    opened_socket.send(response)
+
+
+def get_message(opened_socket, CONFIGS):
+    response = opened_socket.recv(int(CONFIGS.get('MAX_PACKAGE_LENGTH')))
+    if isinstance(response, bytes):
+        json_response = response.decode(CONFIGS.get('ENCODING'))
+        response_dict = json.loads(json_response)
+        if isinstance(response_dict, dict):
+            return response_dict
+        raise ValueError
+    raise ValueError
