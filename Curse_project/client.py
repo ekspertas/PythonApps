775b16aaa228f302c7e935d5ab06e63a0b2d539b
ekspertas <ekspertasd@gmail.com>
2022-02-02 18:19:20 +0200

    дополнение ДЗ к уроку 7

--- Curse_project/client.py
+++ Curse_project/client.py
@@ -2,16 +2,19 @@ import json
 import sys
 import socket
 import time
-
+import argparse
 
 from utils import load_configs, send_message, get_message, log
 from log.client_log_config import client_log
+from log.server_log_config import server_log
+from errors import ReqFieldMissingError, ServerError
 
 CONFIGS = {}
 
 
 @log
-def create_presence_message(account_name, CONFIGS):
+def create_presence_message(CONFIGS, account_name='Guest'):
+    """Функция генерирует запрос о присутствии клиента"""
     message = {
         CONFIGS.get('ACTION'): CONFIGS.get('PRESENCE'),
         CONFIGS.get('TIME'): time.time(),
@@ -23,8 +26,47 @@ def create_presence_message(account_name, CONFIGS):
     return message
 
 
+def get_user_message(sock, CONFIGS, account_name='Guest'):
+    """ Функция запрашивает текст сообщения и возвращает его.
+        Так же завершает работу при вводе комманды '!!!' """
+    message = input('Введите сообщение для отправки или \'!!!\' для завершения работы: ')
+    if message == '!!!':
+        sock.close()
+        client_log.info('Завершение работы по команде пользователя.')
+        print('Спасибо за использование нашего сервиса!')
+        sys.exit(0)
+    message_dict = {
+        CONFIGS['ACTION']: 'message',
+        CONFIGS['MESSAGE']: 'message',
+        CONFIGS['TIME']: time.time(),
+        CONFIGS['ACCOUNT_NAME']: account_name,
+        CONFIGS['MESSAGE_TEXT']: message
+    }
+    client_log.debug(f'Сформирован словарь сообщения: {message_dict}')
+    return message_dict
+
+
+def handle_server_message(message, CONFIG):
+    """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+    if CONFIG['ACTION'] in message and message[CONFIG['ACTION']] == CONFIG['MESSAGE'] and \
+            CONFIG['SENDER'] in message and CONFIG['MESSAGE_TEXT'] in message:
+        print(f'Получено сообщение от пользователя '
+              f'{message[CONFIG["SENDER"]]}:\n{message[CONFIG["MESSAGE_TEXT"]]}')
+        server_log.info(f'Получено сообщение от пользователя '
+                        f'{message[CONFIG["SENDER"]]}:\n{message[CONFIG["MESSAGE_TEXT"]]}')
+    else:
+        client_log.error(f'Получено некорректное сообщение с сервера: {message}')
+
+
 @log
 def handle_response(message, CONFIGS):
+    """
+    Функция разбирает ответ сервера на сообщение о присутствии,
+    возращает 200 если все ОК или генерирует исключение при ошибке
+    """
+    print(message)
+    print('6')
+    client_log.debug(f'Разбор приветственного сообщения от сервера: {message}')
     if CONFIGS.get('RESPONSE') in message:
         if message[CONFIGS.get('RESPONSE')] == 200:
             client_log.info('Успешный запрос')
@@ -34,34 +76,125 @@ def handle_response(message, CONFIGS):
     raise ValueError
 
 
+@log
+def arg_parser(CONFIGS):
+    """Создаём парсер аргументов коммандной строки
+    и читаем параметры, возвращаем 3 параметра
+    """
+    parser = argparse.ArgumentParser()
+    print(parser)
+    parser.add_argument('addr', default=CONFIGS['DEFAULT_IP_ADDRESS'], nargs='?')
+    parser.add_argument('port', default=CONFIGS['DEFAULT_PORT'], type=int, nargs='?')
+    parser.add_argument('-m', '--mode', default='send', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    client_mode = namespace.mode
+
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        client_log.critical('Порт должен быть указан в пределах от 1024 до 65535')
+        sys.exit(1)
+
+    # Проверим допустим ли выбранный режим работы клиента
+    if client_mode not in ('listen', 'send'):
+        server_log.critical(f'Указан недопустимый режим работы {client_mode}, допустимые режимы: listen , send')
+        sys.exit(1)
+
+    return server_address, server_port, client_mode
+
+
 def main():
     global CONFIGS
     CONFIGS = load_configs(is_server=False)
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if not 65535 >= server_port >= 1024:
-            raise ValueError
-    except ValueError:
-        client_log.error('Порт должен быть указан в пределах от 1024 до 65535')
-        sys.exit(1)
-    except IndexError:
-        client_log.info('IP адрес и порт установлены "по умолчанию"')
-        server_address = CONFIGS.get('DEFAULT_IP_ADDRESS')
-        server_port = int(CONFIGS.get('DEFAULT_PORT'))
+    """Загружаем параметы коммандной строки"""
+    server_address, server_port, client_mode = arg_parser(CONFIGS)
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    presence_message = create_presence_message('Guest', CONFIGS)
-    client_log.info('Отправка сообщения на сервер')
-    send_message(transport, presence_message, CONFIGS)
+    # Инициализация сокета и сообщение серверу о нашем появлении
     try:
-        response = get_message(transport, CONFIGS)
-        handled_response = handle_response(response, CONFIGS)
-        client_log.info(f'Ответ от сервера: {response}')
-        client_log.info(f'Обработанный ответ сервера: {handled_response}')
-    except (ValueError, json.JSONDecodeError):
-        client_log.error('Ошибка декодирования сообщения')
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        print('1')
+        send_message(transport, create_presence_message(CONFIGS), CONFIGS)
+        print('2')
+        b = get_message(transport, CONFIGS)
+        print(b)
+        print(10)
+        answer = handle_response(b, CONFIGS)
+        print(client_mode)
+        client_log.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
+        print(f'Установлено соединение с сервером.')
+    except json.JSONDecodeError:
+        client_log.error('Не удалось декодировать полученную Json строку.')
+        sys.exit(1)
+    except ServerError as error:
+        client_log.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    except ReqFieldMissingError as missing_error:
+        client_log.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
+        sys.exit(1)
+    except ConnectionRefusedError:
+        client_log.critical(
+            f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+            f'конечный компьютер отверг запрос на подключение.')
+        sys.exit(1)
+    else:
+        # Если соединение с сервером установлено корректно,
+        # начинаем обмен с ним, согласно требуемому режиму.
+        # основной цикл прогрммы:
+        if client_mode == 'send':
+            print('Режим работы - отправка сообщений.')
+        else:
+            print('Режим работы - приём сообщений.')
+
+        while True:
+            # режим работы - отправка сообщений
+            print(client_mode)
+            if client_mode == 'send':
+                try:
+                    send_message(transport, get_user_message(transport, CONFIGS), CONFIGS)
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    client_log.error(f'Соединение с сервером {server_address} было потеряно.')
+                    sys.exit(1)
+
+            # Режим работы приём:
+            if client_mode == 'listen':
+                try:
+                    handle_server_message(get_message(transport, CONFIGS), CONFIGS)
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    client_log.error(f'Соединение с сервером {server_address} было потеряно.')
+                    sys.exit(1)
+
+
+# def main():
+#     global CONFIGS
+#     CONFIGS = load_configs(is_server=False)
+#     try:
+#         server_address = sys.argv[1]
+#         print(server_address)
+#         server_port = int(sys.argv[2])
+#         if not 65535 >= server_port >= 1024:
+#             raise ValueError
+#     except ValueError:
+#         client_log.error('Порт должен быть указан в пределах от 1024 до 65535')
+#         sys.exit(1)
+#     except IndexError:
+#         client_log.info('IP адрес и порт установлены "по умолчанию"')
+#         server_address = CONFIGS.get('DEFAULT_IP_ADDRESS')
+#         server_port = int(CONFIGS.get('DEFAULT_PORT'))
+#
+#     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     transport.connect((server_address, server_port))
+#     presence_message = create_presence_message(CONFIGS, 'Guest')
+#     client_log.info('Отправка сообщения на сервер')
+#     send_message(transport, presence_message, CONFIGS)
+#     try:
+#         response = get_message(transport, CONFIGS)
+#         handled_response = handle_response(response, CONFIGS)
+#         client_log.info(f'Ответ от сервера: {response}')
+#         client_log.info(f'Обработанный ответ сервера: {handled_response}')
+#     except (ValueError, json.JSONDecodeError):
+#         client_log.error('Ошибка декодирования сообщения')
 
 
 if __name__ == '__main__':
--- Curse_project/server.py
+++ Curse_project/server.py
@@ -1,23 +1,36 @@
+import argparse
 import json
 import socket
 import sys
+import select
+import time
 
 from utils import load_configs, send_message, get_message, log
 from log.server_log_config import server_log
-
 
 CONFIGS = {}
 
 
 @log
-def handle_message(message, CONFIGS):
+def handle_message(message, messages_list, client, CONFIGS):
+    server_log.debug(f'Обработка сообщения от клиента : {message}')
+    # Если это сообщение о присутствии, принимаем и отвечаем
     if CONFIGS.get('ACTION') in message \
             and message[CONFIGS.get('ACTION')] == CONFIGS.get('PRESENCE') \
             and CONFIGS.get('TIME') in message \
             and CONFIGS.get('USER') in message \
             and message[CONFIGS.get('USER')][CONFIGS.get('ACCOUNT_NAME')] == 'Guest':
-        server_log.info('Request received')
+        send_message(client, {CONFIGS.get('RESPONSE'): 200}, CONFIGS)
+        # return
         return {CONFIGS.get('RESPONSE'): 200}
+    # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
+    elif CONFIGS.get('ACTION') in message \
+            and message[CONFIGS.get('ACTION')] == CONFIGS['MESSAGE'] \
+            and CONFIGS.get('TIME') in message \
+            and CONFIGS.get('MESSAGE_TEXT') in message:
+        messages_list.append((message[CONFIGS.get('ACCOUNT_NAME')], message[CONFIGS.get('MESSAGE_TEXT')]))
+        return
+    # Иначе отдаём Bad request
     server_log.error('Bad request')
     return {
         CONFIGS.get('RESPONSE'): 400,
@@ -25,51 +38,112 @@ def handle_message(message, CONFIGS):
     }
 
 
+def arg_parser(CONFIGS):
+    """Создаём парсер аргументов коммандной строки
+    и читаем параметры, возвращаем 2 параметра
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=CONFIGS['DEFAULT_PORT'], type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    # проверим подходящий номер порта
+    if not 1023 < listen_port < 65536:
+        server_log.critical(f'Попытка запуска сервера с некорректного порта {listen_port}.'
+                            'Порт должен быть указан в пределах от 1024 до 65535')
+        sys.exit(1)
+
+    return listen_address, listen_port
+
+
 def main():
     global CONFIGS
     CONFIGS = load_configs()
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = int(CONFIGS.get('DEFAULT_PORT'))
-        if not 65535 >= listen_port >= 1024:
-            raise ValueError
-    except IndexError:
-        server_log.error('После -\'р\' необходимо указать порт')
-        sys.exit(1)
-    except ValueError:
-        server_log.error('Порт должен быть указан в пределах от 1024 до 65535')
-        sys.exit(1)
+    """Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию"""
+    listen_address, listen_port = arg_parser(CONFIGS)
+    server_log.info(
+        f'Запущен сервер, порт для подключений: {listen_port}, '
+        f'адрес с которого принимаются подключения: {listen_address}. '
+        f'Если адрес не указан, принимаются соединения с любых адресов.')
 
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-
-    except IndexError:
-        server_log.error('После -\'а\' необходимо указать адрес для подключения')
-        sys.exit(1)
-
+    # Готовим сокет
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
+    transport.settimeout(0.5)
 
+    # список клиентов , очередь сообщений
+    clients = []
+    messages = []
+
+    # Слушаем порт
     transport.listen(int(CONFIGS.get('MAX_CONNECTIONS')))
-
     server_log.info('Server ONLINE')
 
+    # Основной цикл программы сервера
     while True:
-        client, client_address = transport.accept()
+        # Ждём подключения, если таймаут вышел, ловим исключение.
         try:
-            message = get_message(client, CONFIGS)
-            response = handle_message(message, CONFIGS)
-            send_message(client, response, CONFIGS)
-            client.close()
-            server_log.info('Принято сообщение от клиента')
-        except (ValueError, json.JSONDecodeError):
-            server_log.warning('Принято не корректное сообщение от клиента')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError:
+            pass
+        else:
+            server_log.info(f'Установлено соедение с ПК {client_address}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+
+        # Проверяем на наличие ждущих клиентов
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        # принимаем сообщения и если там есть сообщения,
+        # кладём в словарь, если ошибка, исключаем клиента.
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    handle_message(get_message(client_with_message, CONFIGS), messages, client_with_message, CONFIGS)
+                    c = handle_message(get_message(client_with_message, CONFIGS), messages, client_with_message, CONFIGS)
+                    print(c)
+                    print(8)
+                except:
+                    server_log.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
+                    clients.remove(client_with_message)
+
+        # Если есть сообщения для отправки и ожидающие клиенты, отправляем им сообщение.
+        if messages and send_data_lst:
+            message = {
+                CONFIGS['ACTION']: CONFIGS['MESSAGE'],
+                CONFIGS['SENDER']: messages[0][0],
+                CONFIGS['TIME']: time.time(),
+                CONFIGS['MESSAGE_TEXT']: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    send_message(waiting_client, message, CONFIGS)
+                except:
+                    server_log.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    waiting_client.close()
+                    clients.remove(waiting_client)
+
+    # while True:
+    #     client, client_address = transport.accept()
+    #     try:
+    #         message = get_message(client, CONFIGS)
+    #         response = handle_message(message, CONFIGS)
+    #         send_message(client, response, CONFIGS)
+    #         client.close()
+    #         server_log.info('Принято сообщение от клиента')
+    #     except (ValueError, json.JSONDecodeError):
+    #         server_log.warning('Принято не корректное сообщение от клиента')
+    #         client.close()
 
 
 if __name__ == '__main__':
